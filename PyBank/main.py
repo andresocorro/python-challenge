35bb32f45680bc9012d671f0523016c5ba8258a3
Andres <andressocorro1@gmail.com>
2020-08-16 17:50:01 -0500

    Edited Sum of months. It was breaking total pnl sum, added both now

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,14 +2,27 @@
 import os
 import csv
 
+
 # Go to file with data
 pnl_csv = os.path.join("Resources","budget_data.csv")
 
-# Open file with csvreader
 
-with open(pnl_csv) as csv_file:
+all_rows = []
+total_pnl = 0
+
+# Open file with csvreader
+with open(pnl_csv, newline='') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     
+    csv_header = next(csv_reader)
+    
     for row in csv_reader:
-        total_months = len(list(csv_reader))
-        print(total_months)
+    #  Add rows to get total number of rows
+        all_rows.append(row)
+        total_months = len(all_rows)
+    
+    # Sum pnl column to obtain total change   
+        total_pnl += int(row[1])
+    
+    print(total_months)
+    print(total_pnl)
