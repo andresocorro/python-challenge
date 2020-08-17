a8459ed11a56fc04c6ccf5321de11fd6618106d9
Andres <andressocorro1@gmail.com>
2020-08-16 21:54:49 -0500

    Updated code to make it simpler for next calculations by separating data into separate lists

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,22 +7,28 @@ import csv
 pnl_csv = os.path.join("Resources","budget_data.csv")
 
 
-all_rows = []
-total_pnl = 0
 
 # Open file with csvreader
 with open(pnl_csv, newline='') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     
     csv_header = next(csv_reader)
+
+    # Create lists for each column to split data in two different lists
+    months = []
+    pnl = []
     
     for row in csv_reader:
     #  Add rows to get total number of rows
-        all_rows.append(row)
-        total_months = len(all_rows)
+        months.append(row[0])
+        pnl.append(int(row[1]))
+
+    # Obtain total months of data
+    total_months = len(months)
+    # print(total_months)
+
+    # Obtain total pnl for the time period in question
+    total_pnl = sum(pnl)
+    print(f"${total_pnl}")
+  
     
-    # Sum pnl column to obtain total change   
-        total_pnl += int(row[1])
-    
-    print(total_months)
-    print(total_pnl)
